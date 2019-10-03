f60009dbf33de87b33dcca0f5b05cd7418ddb294
eliasroebl <eliasroebl@gmail.com>
2019-10-03 13:27:48 +0200

    wrote TimeServerResource

---
+++ src/main/java/TimeServerResource.java
@@ -0,0 +1,13 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Path("time")
+public class TimeServerResource {
+
+    @GET
+    public String time(){
+        return "Time: " + LocalDateTime.now();
+    }
+}
